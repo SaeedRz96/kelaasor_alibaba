479a9fa915dfe9ded11de5dba035df6d742b0231
SaeedRz96 <saeedramezani75@gmail.com>
2023-12-14 12:49:02 +0330

    update: django admin panel customized

--- flights/models.py
+++ flights/models.py
@@ -10,10 +10,22 @@ class Airport(models.Model):
     open_time = models.TimeField()
     close_time = models.TimeField()
 
+    def __str__(self) -> str:
+        return self.name
+    
+    class Meta:
+        verbose_name = "Kelaasor Airport"
+
 
 class Flight(models.Model):
     origin = models.ForeignKey(Airport, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    no = models.CharField(max_length=10)
+    no = models.CharField(max_length=10, verbose_name="Code")
     capacity = models.IntegerField()
-    price = models.FloatField()
+    price = models.FloatField(help_text="Price in Rial")
+
+    def __str__(self) -> str:
+        return "{} - {}".format(self.name,self.no)
+
+    class Meta:
+        verbose_name = "Kelaasor Flight"
