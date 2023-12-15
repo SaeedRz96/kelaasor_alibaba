0e26f6cff7f823131e6b63cfdfb7cb275c39476c
SaeedRz96 <saeedramezani75@gmail.com>
2023-12-15 12:05:34 +0330

    update: settings file updated for html render

--- flights/views.py
+++ flights/views.py
@@ -1,29 +1,31 @@
 from django.http.response import HttpResponse, JsonResponse
 from .models import Flight, Airport
+from django.shortcuts import render
+
 
 def list(request):
-    flights = Flight.objects.all()
-    flight_list = []
-    for item in flights:
-        dictionary = {
-            "name" : item.name,
-            "origin" : item.origin.name,
-            "destination" : item.destination.name,
-            "price" : item.price
-        }
-        flight_list.append(dictionary)
-    return JsonResponse(flight_list, safe=False)
+    # flights = Flight.objects.all()
+    # flight_list = []
+    # for item in flights:
+    #     dictionary = {
+    #         "name" : item.name,
+    #         "origin" : item.origin.name,
+    #         "destination" : item.destination.name,
+    #         "price" : item.price
+    #     }
+    #     flight_list.append(dictionary)
+    return render(request, 'flights/list.html')
 
 
 def list2(request):
-    airports = Airport.objects.all()
-    airport_list = []
-    for item in airports:
-        airport_dict = {
-            "name" : item.name,
-            "no" : item.no,
-            "city" : item.city
-        }
-        airport_list.append(airport_dict)
+    # airports = Airport.objects.all()
+    # airport_list = []
+    # for item in airports:
+    #     airport_dict = {
+    #         "name" : item.name,
+    #         "no" : item.no,
+    #         "city" : item.city
+    #     }
+    #     airport_list.append(airport_dict)
     
-    return HttpResponse(airport_list)
+    return render(request, 'flights/list2.html')
