f8f0da87eacd6fc8b14b361ed23f5f5a2cd0abc2
SaeedRz96 <saeedramezani75@gmail.com>
2023-12-15 13:05:46 +0330

    update: html file added

--- flights/views.py
+++ flights/views.py
@@ -4,28 +4,22 @@ from django.shortcuts import render
 
 
 def list(request):
-    # flights = Flight.objects.all()
-    # flight_list = []
-    # for item in flights:
-    #     dictionary = {
-    #         "name" : item.name,
-    #         "origin" : item.origin.name,
-    #         "destination" : item.destination.name,
-    #         "price" : item.price
-    #     }
-    #     flight_list.append(dictionary)
-    return render(request, 'flights/list.html')
+    flights = Flight.objects.all()
+    f_list = {
+        'flights' : flights
+    }
+    return render(request, 'flights/list.html', context=f_list)
 
 
 def list2(request):
-    # airports = Airport.objects.all()
-    # airport_list = []
-    # for item in airports:
-    #     airport_dict = {
-    #         "name" : item.name,
-    #         "no" : item.no,
-    #         "city" : item.city
-    #     }
-    #     airport_list.append(airport_dict)
+    airports = Airport.objects.all()
+    airport_list = []
+    for item in airports:
+        airport_dict = {
+            "name" : item.name,
+            "no" : item.no,
+            "city" : item.city
+        }
+        airport_list.append(airport_dict)
     
-    return render(request, 'flights/list2.html')
+    return HttpResponse(airport_list)
